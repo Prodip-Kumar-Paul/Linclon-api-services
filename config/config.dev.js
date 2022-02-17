614a7a3e0ab3e55d88b0adcf759c00e867da32c6
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2022-02-17 20:34:12 +0530

    code improvement

--- config/config.dev.js
+++ config/config.dev.js
@@ -1,19 +1,26 @@
 import dotenv from "dotenv";
-dotenv.config();
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({
+   path: path.join(__dirname, `config.${process.env.NODE_ENV.trim()}.env`),
+});
 
 const configDev = {
-   HOST: process.env.DEV_HOST,
-   PORT: process.env.DEV_PORT,
-   DB_NAME: process.env.DEV_DB_NAME,
-   DB_URL: process.env.DEV_DB_URL,
-   DB_PASSWORD: process.env.DEV_DB_PASSWORD,
+   HOST: process.env.HOST,
+   PORT: process.env.PORT,
+   DB_NAME: process.env.DB_NAME,
+   DB_URL: process.env.DB_URL,
+   DB_PASSWORD: process.env.DB_PASSWORD,
    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
    GITHUB_CLIENTID_ID: process.env.GITHUB_CLIENTID_ID,
    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
    GITHUB_REDIRECT_URL: process.env.GITHUB_REDIRECT_URL,
-   CLOUDINARY_CLOUD_NAME:process.env.CLOUDINARY_CLOUD_NAME,
-   CLOUDINARY_API_KEY:process.env.CLOUDINARY_API_KEY,
-   CLOUDINARY_API_SECRET:process.env.CLOUDINARY_API_SECRET
+   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
 
 export default configDev;
