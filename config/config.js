26623ccbd502dc3f76e5f7edec22f2f6e67230b0
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2022-02-24 19:34:33 +0530

    revert prev changes

--- config/config.js
+++ config/config.js
@@ -6,11 +6,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 dotenv.config({
-   path: path.join(__dirname, `config.${process.env.NODE_ENV.trim()}.env`),
+   path: path.join(
+      __dirname,
+      `config.${
+         process.env.NODE_ENV ? process.env.NODE_ENV.trim() : "prod"
+      }.env`
+   ),
 });
 // console.log(
 //    "ENV file path --> ",
-//    path.join(__dirname, `config.${process.env.NODE_ENV.trim()}.env`)
+//    path.join(__dirname, `config.${process.env.NODE_ENV?process.env.NODE_ENV.trim():"prod"}.env`)
 // );
 
 import configDev from "./config.dev.js";
@@ -18,9 +23,11 @@ import configProd from "./config.prod.js";
 
 let config = {};
 
-if (process.env.NODE_ENV.trim() === "dev") {
+if (process.env.NODE_ENV === "dev ") {
    config = { ...configDev };
-} else if (process.env.NODE_ENV.trim() === "prod") {
+} else if (process.env.NODE_ENV === "prod ") {
+   config = { ...configProd };
+} else {
    config = { ...configProd };
 }
 // console.log(config);
